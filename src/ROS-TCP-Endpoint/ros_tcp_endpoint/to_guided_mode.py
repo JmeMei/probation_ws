d8ad8d283614b25c0e7842f59bc8e1eb85b48bdb
Jamie002 <Jamie002@e.ntu.edu.sg>
2025-10-04 10:59:13 +0800

    to_guided_mode

--- src/ROS-TCP-Endpoint/ros_tcp_endpoint/to_guided_mode.py
+++ src/ROS-TCP-Endpoint/ros_tcp_endpoint/to_guided_mode.py
@@ -3,21 +3,23 @@ from rclpy.node import Node
 from mavros_msgs.srv import SetMode
 
 class ModeClient(Node):
+    MAV_MODE_GUIDED_ARMED = 216
+    
     def __init__(self):
         super().__init__('mode')
-        self.declare_parameter('mode', 'GUIDED')
-        self.mode_cli = self.create_client(SetMode, '/mavros/set_mode')
+        #self.declare_parameter('mode', 'GUIDED')
+        self.mode_client = self.create_client(SetMode, '/mavros/set_mode')
 
     def call_change_mode(self, mode):
-        while not self.mode_cli.wait_for_service(timeout_sec=1.0):
+        while not self.mode_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().warn("Waiting for MAVROS set_mode service...")
         
         # Set mode
         mode_req = SetMode.Request()
-        mode_req.base_mode = 0
+        mode_req.base_mode = self.MAV_MODE_GUIDED_ARMED  # Use the constant
         mode_req.custom_mode = mode
         self.get_logger().info(f'Setting mode: {mode}')
-        mode_future = self.mode_cli.call_async(mode_req)
+        mode_future = self.mode_client.call_async(mode_req)
 
         self.get_logger().info('Mode change request sent successfully')
 
@@ -26,11 +28,46 @@ def main():
     node = ModeClient()
     mode = node.get_parameter('mode').value
     node.call_change_mode(mode)
-    rclpy.spin_once(node)  # Process the service calls
+    rclpy.spin_once(node)  # so that you don't have to ctrl-c
     node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
-            
+
+"""
+ Commands that I have used to check the mode:
+- ros2 service list # check available services
+
+- ros2 service type /mavros/set_mode # check service type
+
+- ros2 interface show mavros_msgs/srv/SetMode
+
+ # set FCU mode
+#
+# Known custom modes listed here:
+# http://wiki.ros.org/mavros/CustomModes
+
+# basic modes from MAV_MODE
+uint8 MAV_MODE_PREFLIGHT                = 0
+uint8 MAV_MODE_STABILIZE_DISARMED       = 80
+uint8 MAV_MODE_STABILIZE_ARMED          = 208
+uint8 MAV_MODE_MANUAL_DISARMED          = 64
+uint8 MAV_MODE_MANUAL_ARMED             = 192
+uint8 MAV_MODE_GUIDED_DISARMED          = 88
+uint8 MAV_MODE_GUIDED_ARMED             = 216
+uint8 MAV_MODE_AUTO_DISARMED            = 92
+uint8 MAV_MODE_AUTO_ARMED               = 220
+uint8 MAV_MODE_TEST_DISARMED            = 66
+uint8 MAV_MODE_TEST_ARMED               = 194
+
+uint8 base_mode         # filled by MAV_MODE enum value or 0 if custom_mode != ''
+string custom_mode      # string mode representation or integer
+---
+bool mode_sent          # Mode known/parsed correctly and SET_MODE are sent
+
+If you want to check the current mode using the command line, you can use:
+- ros2 service call /mavros/cmd/arming mavros_msgs/srv/CommandBool "{value: true}" 
+"""
+
 
